85d7a20280800f3c295bbd34b001c08067057def
espeon3 <espeonicalic2@gmail.com>
2026-08-10 20:51:47 +0200

    Update VultureEvents.cs

--- TownOfExtra/Events/VultureEvents.cs
+++ TownOfExtra/Events/VultureEvents.cs
@@ -52,73 +52,7 @@ public class VultureEvents
         if (PlayerControl.LocalPlayer == e.Player)
         {
             var notif = Helpers.CreateAndShowNotification(
-                $"You have {TownOfExtraColours.VultureRoleColour.ToTextColor()}eaten</color> a body and are now at {TownOfUsColors.Neutral.ToTextColor()}{VultureRole.DeadBodiesEaten}/{OptionGroupSingleton<VultureRoleOptions>.Instance.EatenBodiesNeeded}</color> bodies!",
+                $"You have {TownOfExtraColours.VultureRoleColour.ToTextColor()}eaten</color> a body and are now at {TownOfUsColors.Neutral.ToTextColor()}{VultureRole.DeadBodiesEaten}/{OptionGroupSingleton<VultureRoleOptions>.Instance.EatenBodiesNeeded}</color> bodies digested!",
                 Color.white, new Vector3(0f, 1f, -20f), spr: TownOfExtraAssets.VultureEatButton.LoadAsset());
             notif.AdjustNotification();
         }
-
-        if (AmongUsClient.Instance.AmHost)
-        {
-            var winners = CustomRoleUtils.GetActiveRolesOfType<VultureRole>()
-                .Where(t => t.WinConditionMet()).ToList();
-
-            if (winners.Count > 0)
-            {
-                CustomGameOver.Trigger<NeutralGameOver>(winners.Select(v => v.Player.Data).ToList());
-            }
-            else
-            {
-                var vulture = CustomRoleUtils.GetActiveRolesOfType<VultureRole>()
-                    .FirstOrDefault(v =>
-                        !v.Player.HasDied() && 
-                        VultureRole.DeadBodiesEaten >= OptionGroupSingleton<VultureRoleOptions>.Instance.EatenBodiesNeeded && 
-                        OptionGroupSingleton<VultureRoleOptions>.Instance.WinType is VultureWinType.LeaveInVictory);
-                
-                if (vulture == null) return;
-                
-                foreach (var plr in PlayerControl.AllPlayerControls)
-                {
-                    if (vulture.Player == plr)
-                    {
-                        plr.RpcSendNotification(
-                            $"You have successfully won as the {TownOfExtraColours.VultureRoleColour.ToTextColor()}Vulture</color>, as you have eaten enough bodies!",
-                            "VultureEatButton",
-                            "NeutButton"
-                        );
-                    }
-                    else
-                    {
-                        plr.RpcSendNotification(
-                            $"The {TownOfExtraColours.VultureRoleColour.ToTextColor()}Vulture</color>, {vulture.Player.Data.PlayerName}, has won, as they have eaten enough bodies!",
-                            "VultureEatButton",
-                            "NeutButton"
-                        );
-                    }
-                }
-
-                DeathHandlerModifier.UpdateDeathHandlerImmediate(vulture.Player, TouLocale.Get("DiedToWinning"),
-                    DeathEventHandlers.CurrentRound, DeathHandlerOverride.SetFalse,
-                    lockInfo: DeathHandlerOverride.SetTrue);
-                
-                vulture.Player.Exiled();
-            }
-        }
-    }
-
-    private static void CheckAndConvertVulture()
-    {
-        if (!OptionGroupSingleton<VultureRoleOptions>.Instance.TurnIntoAmne) return;
-        
-        int impostors = 0;
-        int others = 0;
-
-        foreach (var p in PlayerControl.AllPlayerControls)
-        {
-            if (p.Data.IsDead || p.GetTownOfUsRole() is VultureRole) continue;
-            if (p.IsImpostor()) impostors++;
-            else others++;
-        }
-
-        VultureRpcs.RpcChangeVultureToAmne(others, impostors);
-    }
-}
