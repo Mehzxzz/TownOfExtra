d46aa479aae2bfbe26c9db2730a6ae8969a7df78
espeon3 <espeonicalic2@gmail.com>
2026-08-10 21:33:40 +0200

    Create VultureKillButton.cs

---
+++ TownOfExtra/Buttons/VultureKillButton.cs
@@ -0,0 +1,45 @@
+using MiraAPI.GameOptions;
+using MiraAPI.Keybinds;
+using MiraAPI.Networking;
+using MiraAPI.Utilities.Assets;
+using TownOfExtra.Options.Roles;
+using TownOfExtra.Roles.Neutral.Killing;
+using TownOfUs;
+using TownOfUs.Buttons;
+using TownOfUs.Networking;
+using TownOfUs.Options.Modifiers.Alliance;
+using TownOfUs.Utilities;
+using UnityEngine;
+
+namespace TownOfExtra.Buttons;
+
+public sealed class MurdererMurderButton : TownOfUsKillRoleButton<MurdererRole, PlayerControl>, IDiseaseableButton, IKillButton
+{
+    public override string Name => "Murder";
+    public override BaseKeybind Keybind => Keybinds.PrimaryAction;
+    public override Color TextOutlineColor => TownOfUsColors.Neutral;
+    public override float Cooldown => OptionGroupSingleton<MurdererRoleOptions>.Instance.MurderCooldown;
+    public override LoadableAsset<Sprite> Sprite => TownOfExtraAssets.AttackPh;
+
+    public void SetDiseasedTimer(float multiplier)
+    {
+        SetTimer(Cooldown * multiplier);
+    }
+
+    protected override void OnClick()
+    {
+        if (Target == null) return;
+
+        PlayerControl.LocalPlayer.RpcSpecialMurder(Target, MeetingCheck.OutsideMeeting, causeOfDeath: "Murdered");
+    }
+
+    public override PlayerControl GetTarget()
+    {
+        if (!OptionGroupSingleton<LoversOptions>.Instance.LoversKillEachOther && PlayerControl.LocalPlayer.IsLover())
+        {
+            return PlayerControl.LocalPlayer.GetClosestLivingPlayer(true, Distance, false, x => !x.IsLover());
+        }
+
+        return PlayerControl.LocalPlayer.GetClosestLivingPlayer(true, Distance);
+    }
+}
