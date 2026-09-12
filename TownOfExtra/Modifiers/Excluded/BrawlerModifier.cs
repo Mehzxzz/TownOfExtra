b289ef25ffb573a045e7b0f8be61d4ebd5f6b757
Mehzxzz <243104136+Mehzxzz@users.noreply.github.com>
2026-09-12 11:16:42 +0100

    removed unused modifiers

--- TownOfExtra/Modifiers/Excluded/BrawlerModifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using MiraAPI.Modifiers;
-
-namespace TownOfExtra.Modifiers.Excluded;
-
-public class BrawlerModifier : BaseModifier
-{
-    public override string ModifierName => "Brawler";
-    public override bool HideOnUi => true;
-}
